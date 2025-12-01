c197902e85cf3e15aa59f4de57092cc064bc3521
angelostori <angelostori13@gmail.com>
2025-12-01 10:34:39 +0100

    add terms data and update terms routes for CRUD operations

---
+++ data/terms.js
@@ -0,0 +1,13 @@
+const terms = [
+
+    { id: '1', term: 'API', definition: 'Application Programming Interface' },
+
+    { id: '2', term: 'REST', definition: 'Representational State Transfer' },
+
+    { id: '3', term: 'CRUD', definition: 'Create, Read, Update, Delete' },
+
+    { id: '4', term: 'Express', definition: 'A minimal and flexible Node.js web application framework' }
+
+]
+
+module.exports = terms
--- routes/terms.js
+++ routes/terms.js
@@ -6,7 +6,26 @@ router.get('/', (req, res) => {
     res.send('Show all terms here')
 })
 
+// show
+router.get('/:id', (req, res) => {
+    res.send('Show terms id ' + req.params.id)
+})
 
+// store
+router.post('/:id', (req, res) => {
+    console.log(req.body);
+    res.send('Store terms')
+})
+
+//update
+router.put('/:id', (req, res) => {
+    res.send('Update terms id ' + req.params.id)
+})
+
+// destroy
+router.delete('/:id', (req, res) => {
+    res.send('Destroy terms id ' + req.params.id)
+})
 
 // export your router instance
 module.exports = router
